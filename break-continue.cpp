276615df9bf39fb3781ffbefc3633e64d05bfec9
haniframadhani <69223521+haniframadhani@users.noreply.github.com>
2021-10-05 12:06:31 +0800

    break & continue

---
+++ break-continue.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i = 0;
+	while (i <= 10)
+	{
+		i++;
+		if (i == 5)
+		{
+			continue;
+			// break;
+		}
+		cout << i << endl;
+	}
+	cout << "akhir" << endl;
+
+	return 0;
+}
