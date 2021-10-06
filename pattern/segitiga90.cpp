651a4012a941e223eeb3c689ae1610d1ae192886
haniframadhani <69223521+haniframadhani@users.noreply.github.com>
2021-10-06 12:32:55 +0800

    pola segitiga siku-siku

---
+++ pattern/segitiga90.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout << "masukkan panjang pola : ";
+	cin >> n;
+
+	cout << "pola 1\n";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	cout << "pola2 2\n";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = n; j >= i; j--)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
