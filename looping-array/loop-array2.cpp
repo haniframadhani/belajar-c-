c46a571062dd5742185f3053729a452e5c48bdf2
haniframadhani <69223521+haniframadhani@users.noreply.github.com>
2021-12-09 13:18:18 +0800

    looping array

---
+++ looping-array/loop-array2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <array>
+using namespace std;
+int main()
+{
+	// looping untuk array di c++ 11 keatas
+	/*
+	for(deklarasi variabel : array) {
+			statement;
+	}
+	*/
+	array<int, 10> arrayNilai = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	for (int nilai : arrayNilai)
+	{
+		cout << "address : " << &nilai << " nilainya : " << nilai << endl;
+		nilai = 1; // tidak merubah array
+	}
+	cout << "\n";
+	// memanipulasi array dengan referensi
+	for (int &nilaiRef : arrayNilai)
+	{
+		nilaiRef *= 2;
+	}
+	cout << "\n";
+	for (int &nilaiRef : arrayNilai)
+	{
+		cout << "address : " << &nilaiRef << " nilainya : " << nilaiRef << endl;
+	}
+
+	return 0;
+}
