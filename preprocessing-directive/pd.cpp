990ec1a107dd879aee13696c06a3bd4636dcb4b7
haniframadhani <69223521+haniframadhani@users.noreply.github.com>
2022-01-06 08:42:25 +0800

    prepocessing directive

---
+++ preprocessing-directive/pd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// macro
+#define PI 3.141592653589
+
+using namespace std;
+
+double pi = 3.141592653589;
+int main()
+{
+    cout << "nilai double pi = " << pi << endl;
+    cout << &pi << endl;
+    cout << "nilai PI = " << PI << endl;
+
+    return 0;
+}
